dcc47e4f0fcd8998cd505945e0c4b2c882f8f8a8
Денис <Sosnin.den@gmail.com>
2022-04-02 17:15:38 +0300

    home work c1 lesson 1 complited

--- home_work_c1_01/hw_c1_01_2.py
+++ home_work_c1_01/hw_c1_01_2.py
@@ -2,7 +2,15 @@
 # Пользователь вводит время в секундах.
 # Переведите время в часы, минуты, секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
 
-initial_time = int(input('Введите количество секунд '))
+while True:
+    initial_time = input('Введите количество секунд: ')
+    try:
+        initial_time.isnumeric()
+        initial_time = int(initial_time)
+        break
+    except:
+        print('Вы ввели неправильный формат данных.')
+
 time_hour = initial_time // 3600
 time_minute = (initial_time % 3600) // 60
 time_second = (initial_time % 3600) % 60
